99c635da480c02a63bfbfee201f823ca6f36a401
srinibasu <sri.basu@hotmail.com>
2024-02-04 19:49:22 +0530

    changing the name of the eks-cluster

--- EKS-Cluster-Setup/main.tf
+++ EKS-Cluster-Setup/main.tf
@@ -37,22 +37,22 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "my-cluster"
+  cluster_name    = "my-eks-cluster"
   cluster_version = "1.24"
 
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access  = true
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  # EKS Managed Node Group(s)
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  
+    # EKS Managed Node Group(s)
 
   eks_managed_node_groups = {
 
     nodes = {
-      min_size       = 1
-      max_size       = 3
-      desired_size   = 2
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
       instance_types = ["t2.small"]
     }
   }
@@ -62,4 +62,3 @@ module "eks" {
     Terraform   = "true"
   }
 }
-
